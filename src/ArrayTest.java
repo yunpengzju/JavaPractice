5650a66c73a96c7be1fbe7fe74bad29d1c6c0dde
turkey <mathcyp2008@hotmail.com>
2013-09-01 11:23:18 +0800

    数组初始化（长度可以是变量）

---
+++ src/ArrayTest.java
@@ -0,0 +1,10 @@
+
+public class ArrayTest {
+	public static void main(String[] args){
+		double[] array;
+		int NUMBER_OF_ELEMENTS = 100;
+		NUMBER_OF_ELEMENTS = 4;
+		array = new double[NUMBER_OF_ELEMENTS];  // 初始化数组时，数组大小可以是变量
+		System.out.println(array.length);
+	}
+}
