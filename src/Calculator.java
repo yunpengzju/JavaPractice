35197a09e82559cb30a09e259fcf06f6c5c91344
turkey <mathcyp2008@hotmail.com>
2013-09-20 19:11:45 +0800

    重写了java课程逆波兰计算机的代码

---
+++ src/Calculator.java
@@ -0,0 +1,106 @@
+import java.util.*;
+public class Calculator {
+	private String str;
+	private int[] number;
+	private char[] operator;
+	private int number_top;
+	private int op_top;
+	
+	public Calculator(String str){
+		this.str = str;
+		number = new int[100];
+		operator = new char[100];
+		number_top = 0;
+		op_top = 0;
+	}
+	public void pushNum(int a){
+		number[number_top++] = a;
+	}
+	public void pushOp(char a){
+		operator[op_top++] = a;
+	}
+	public int popNum(){
+		return number[--number_top];
+	}
+	public char popOp(){
+		return operator[--op_top];
+	}
+	public char nextOp(){
+		return operator[op_top-1];
+	}
+	public boolean hasNext(){
+		return op_top > 0;
+	}
+	public boolean isNumber(char a){
+		if(a >= '0' && a <= '9')  return true;
+		else return false;
+	}
+	public void computeStep(){
+		char op  = this.popOp();
+		int  op2 = this.popNum();
+		int  op1 = this.popNum();
+		switch(op){
+			case '+': this.pushNum(op1+op2);break;
+			case '-': this.pushNum(op1-op2);break;
+			case '*': this.pushNum(op1*op2);break;
+			case '/': this.pushNum(op1/op2);
+		}
+	}
+	public void computeSeveralStep(){
+		while(this.nextOp() != '('){
+			this.computeStep();
+		}
+		this.popOp();
+	}
+	public int compute(){
+		boolean flag = false;
+		String temp = "";
+		for (int i = 0; i < str.length(); i++){
+			if (this.isNumber(str.charAt(i))) {
+				flag = true;
+				temp += str.charAt(i);
+			}
+			else{
+				if(flag){
+					this.pushNum(Integer.parseInt(temp));
+					temp = "";
+				}
+				flag = false;
+				if(str.charAt(i) == '*' || str.charAt(i) == '/'|| str.charAt(i) == '(')
+					pushOp(str.charAt(i));
+				else if (str.charAt(i) == '+' || str.charAt(i) == '-'){
+					if(this.hasNext()){
+						if(this.nextOp() != '(')
+						this.computeStep();
+					}
+					pushOp(str.charAt(i));
+				}
+				else{
+					this.computeSeveralStep();
+				}
+			}
+		}
+		if(flag) this.pushNum(Integer.parseInt(temp));
+		while(this.hasNext())
+		{
+			this.computeStep();
+		}
+		return this.popNum();
+	}
+
+	public void display(){
+		for(int i = 0; i<number.length; i++)
+			System.out.print(number[i]+" ");
+		System.out.println();
+		System.out.println(operator);
+	}
+	public static void main(String[] args){
+		Scanner input = new Scanner(System.in);
+		String str = input.next();
+		Calculator c = new Calculator(str);
+		System.out.println(c.compute());	
+	}
+}
+
+
+
